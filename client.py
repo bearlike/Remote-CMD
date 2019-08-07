4948b8bd344becad9e70c1a1486224f813e9bdf9
Krishnakanth Alagiri <39209037+bearlike@users.noreply.github.com>
2019-08-08 00:11:06 +0530

    Add system argument support

--- client.py
+++ client.py
@@ -1,35 +1,63 @@
-import time, socket, sys, os
-os.system("cls")
-print('Client Server...')
-time.sleep(1)
-#Get the hostname, IP Address from socket and set Port
-soc = socket.socket()
-shost = socket.gethostname()
-ip = socket.gethostbyname(shost)
-#get information to connect with the server
-print(shost, '({})'.format(ip))
-server_host = "192.168.56.1" #input('Enter server\'s IP address:')
-name = "kk" #input('Enter Client\'s name: ')
-port = 1234
-print('Trying to connect to the server: {}, ({})'.format(server_host, port))
-time.sleep(1)
-soc.connect((server_host, port))
-print("Connected to Server.\n")
-soc.send(name.encode())
-server_name = soc.recv(1024)
-server_name = server_name.decode()
-print('Enter [quit] to exit.')
-while True:
-   message = soc.recv(1024)
-   message = message.decode()
-   if(message != "ACK"):
-      print(server_name, ">", message)
-   message = input(str("Me > "))
-   if message == "[quit]":
+import time, socket, sys, os, getopt
+
+def main(server_host,name):
+   os.system("cls")
+   print('Client Server...')
+   time.sleep(1)
+   #Get the hostname, IP Address from socket and set Port
+   soc = socket.socket()
+   shost = socket.gethostname()
+   ip = socket.gethostbyname(shost)
+   #get information to connect with the server
+   print(shost, '({})'.format(ip))
+   port = 1234
+   print('Trying to connect to the server: {}, ({})'.format(server_host, port))
+   time.sleep(1)
+   soc.connect((server_host, port))
+   print("Connected to Server.\n")
+   soc.send(name.encode())
+   server_name = soc.recv(1024)
+   server_name = server_name.decode()
+   print('Enter [quit] to exit.')
+   while True:
+      message = soc.recv(1024)
+      message = message.decode()
+      if(message != "ACK"):
+         print(server_name, ">", message)
+      message = input(str("Me > "))
+      if message == "[quit]":
+         soc.send(message.encode())
+         print("\n")
+         break
+      elif message == "cls":
+         os.system("cls")
+         print("Enter [quit] to exit.\n")
       soc.send(message.encode())
-      print("\n")
-      break
-   elif message == "cls":
-      os.system("cls")
-      print("Enter [quit] to exit.\n")
-   soc.send(message.encode())
+
+
+# Prints usage/help
+def usageHelp():
+   class TextColors:
+      HEADER = '\033[95m'
+      OKBLUE = '\033[94m'
+      OKGREEN = '\033[92m'
+      WARNING = '\033[93m'
+      FAIL = '\033[91m'
+      ENDC = '\033[0m'
+      BOLD = '\033[1m'
+      UNDERLINE = '\033[4m'
+   print("Usage:\n  client.py ","<IPv4 Address of the server> <client preferred name>")
+   sys.exit(2)
+
+
+if __name__=="__main__":
+   try:
+      server_host, name = sys.argv[1:]
+   except:
+      usageHelp()
+   try:
+      socket.inet_aton(server_host)
+   except socket.error:
+      print("Invalid IP Address or Connection terminated")
+      usageHelp()
+   main(server_host,name)
